4b8e14cb5b9e060358337b20de756b08bc52b3f1
rstuhlmuller <rodmans@aviatrix.com>
2023-12-07 16:25:05 -0800

    fix: scaling name prefix

--- main.tf
+++ main.tf
@@ -64,7 +64,7 @@ resource "aws_ecs_service" "runner" {
 module "ecs-service-autoscaling" {
   source           = "git::https://github.com/cn-terraform/terraform-aws-ecs-service-autoscaling.git?ref=1.0.6"
   for_each         = var.runners
-  name_prefix      = "gh-runner"
+  name_prefix      = each.key
   ecs_cluster_name = aws_ecs_cluster.github_runner_cluster.name
   ecs_service_name = aws_ecs_service.runner[each.key].name
 }
